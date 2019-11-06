27de0c58075b431a047937fff9df5f6682ed2df0
Roan Lindeboom <roa_lindeboom@hotmail.com>
2019-11-06 11:48:04 +0100

    foto change

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-
 var dt = new Date();
-document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));``
+document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "." + (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (dt.getFullYear()) + " " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2));
+``
 
 function myFunction(x) {
     x.classList.toggle("fa-thumbs-down");
-  } 
-  
+}
 
-function like(){
+
+function like() {
     document.getElementById("up").style.width = "200px";
     document.getElementById("up").style.height = "100px";
     document.getElementById("up").style.backgroundColor = "green";
 
     document.getElementById("down").style.width = "115px";
     document.getElementById("down").style.height = "50px";
-    document.getElementById("down").style.backgroundColor = "black"; 
+    document.getElementById("down").style.backgroundColor = "black";
 }
-function dislike(){
+
+function dislike() {
     document.getElementById("down").style.width = "200px";
     document.getElementById("down").style.height = "100px";
-    document.getElementById("down").style.backgroundColor = "red";  
+    document.getElementById("down").style.backgroundColor = "red";
 
     document.getElementById("up").style.width = "115px";
     document.getElementById("up").style.height = "50px";
     document.getElementById("up").style.backgroundColor = "black";
-
 }
 
-  
+var teller = 0;
+function next() {
+
+    
+
+    switch (teller) {
+        case 0:
+            document.getElementById("anime").src = "image/IMG_20181124_142937_902.jpg";
+
+            break;
+        case 1:
+
+            document.getElementById("anime").src = "image/Codes.jpg";
+            break;
+        case 2:
+
+            document.getElementById("anime").src = "image/IMG-20181004-WA0007.jpg";
+            break;
+        default:
+            break;
+    }
+
+    teller++;
+}
